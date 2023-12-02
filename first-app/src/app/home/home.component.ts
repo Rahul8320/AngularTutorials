0732d3bf423f4c5c4d02dcfc7ada6a5d999b7437
Rahul Dey <rahuldey832000@gmail.com>
2023-12-02 14:25:54 +0530

    Add: housing location component

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
