d71ac29b5da1eea5524c64630cf58953c78da22a
Rahul Dey <rahuldey832000@gmail.com>
2023-12-05 22:38:39 +0530

    Add routing

--- ecommerce/src/app/app.routes.ts
+++ ecommerce/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './core/pages/home/home.component';
+import { CategoryComponent } from './core/pages/category/category.component';
+import { CartComponent } from './core/pages/cart/cart.component';
+import { WishlistComponent } from './core/pages/wishlist/wishlist.component';
+import { DetailsComponent } from './core/pages/details/details.component';
+import { RegisterComponent } from './core/pages/register/register.component';
+import { LoginComponent } from './core/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'category',
+    component: CategoryComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+  {
+    path: 'wishlist',
+    component: WishlistComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
--- ecommerce/src/app/shared/services/wishlist.service.ts
+++ ecommerce/src/app/shared/services/wishlist.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class WhistlistService {
+export class WishlistService {
 
   constructor() { }
 }
