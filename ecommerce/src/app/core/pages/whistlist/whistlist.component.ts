1704a9c2bef2dcf9f00778338013df20ac2d1917
Rahul Dey <rahuldey832000@gmail.com>
2023-12-05 22:25:47 +0530

    add: models, services, pages

---
+++ ecommerce/src/app/core/pages/whistlist/whistlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whistlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './whistlist.component.html',
+  styleUrl: './whistlist.component.css'
+})
+export class WhistlistComponent {
+
+}
